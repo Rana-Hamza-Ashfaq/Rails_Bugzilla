91a487ef2c1d5879df85c7d8f63006b7dbb273c7
Rana-Hamza-Ashfaq <hamzaashfaq629@gmail.com>
2021-08-10 00:03:04 +0000

    Change mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,12 +91,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = {:host => 'railsbugzilla.herokuapp.com'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'https://railsbugzilla.herokuapp.com/',
+    domain: 'railsbugzilla.herokuapp.com',
     user_name: 'hamza.ashfaq@devsinc.com',
     password: 'zvcqiwojwbfodvnr',
     authentication: 'plain',
